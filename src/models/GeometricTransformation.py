6e95e8a684cf7958e7c099482d1a1a019c863c11
Matheus <matheus.vex@gmail.com>
2022-05-01 20:29:58 -0300

    refactor(models): lint code

--- src/models/GeometricTransformation.py
+++ src/models/GeometricTransformation.py
@@ -10,10 +10,10 @@ class GeometricTransformation:
         cos = math.cos(math.radians(angle))
         
         if axel == "x" :
-            vertex.y,vertex.z=(vertex.z*sen)+(vertex.y*cos) ,(vertex.z*cos)-(vertex.y*sen)
+            vertex.y, vertex.z = (vertex.z * sen) + (vertex.y * cos) ,(vertex.z * cos) - (vertex.y * sen)
 
         if axel == "y":
-            vertex.x,vertex.z=(vertex.x*cos)+(vertex.z*sen), (vertex.z*cos)-(vertex.x*sen)
+            vertex.x, vertex.z = (vertex.x * cos) + (vertex.z * sen), (vertex.z * cos) - (vertex.x*sen)
 
         if axel == "z":
             vertex.y, vertex.x = (vertex.x * sen) + (vertex.y * cos), (vertex.x * cos) - (vertex.y * sen)
